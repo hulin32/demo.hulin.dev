1eb525f207f695c7fa1982198ad09699ff57e88a
Lin <hulin2021@gmail.com>
2024-08-31 08:26:58 +0200

    update readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link"
 
 export default function Home() {
   return (
+    <div>
+      <Link href="/foo">Foo</Link>
     <div className="w-96 h-48 bg-gradient-to-tr rounded-md from-indigo-600 via-pink-600 to-purple-600 p-0.5">
       <div className="w-full h-full rounded-md bg-white flex items-center justify-center">
         <p className="text-2xl font-manrope font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 via-pink-600 to-purple-600">
@@ -8,5 +11,6 @@ export default function Home() {
         </p>
       </div>
     </div >
+    </div>
   );
 }

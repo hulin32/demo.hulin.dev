d40537de851abe20c738079a1fb91d6a749f00a8
Lin <hulin2021@gmail.com>
2024-08-31 08:56:41 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Home() {
   return (
     <div>
-      <Link href="/foo">Foo</Link>
+      <Link href="/foo">FooLink</Link>
     <div className="w-96 h-48 bg-gradient-to-tr rounded-md from-indigo-600 via-pink-600 to-purple-600 p-0.5">
       <div className="w-full h-full rounded-md bg-white flex items-center justify-center">
         <p className="text-2xl font-manrope font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 via-pink-600 to-purple-600">

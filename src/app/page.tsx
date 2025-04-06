43df756842496a1c9f6c169cec62af2f47e61cb8
lin <hulin2021@gmail.com>
2025-04-06 16:11:03 +0200

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 import Link from 'next/link';
+import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
   return (
     <div>
       <Link href="/foo">FooLink</Link>
-      <div className="h-48 w-96 rounded-md bg-gradient-to-tr from-indigo-600 via-pink-600 to-purple-600 p-0.5">
-        <div className="flex h-full w-full items-center justify-center rounded-md bg-white">
-          <p className="font-manrope bg-gradient-to-r from-indigo-600 via-pink-600 to-purple-600 bg-clip-text text-2xl font-bold text-transparent">
-            Demo
-          </p>
-        </div>
-      </div>
+      <Progress value={33} />
     </div>
   );
 }
